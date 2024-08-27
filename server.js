4a672eefc326ec0ef566fb04cf1816510a1fd46e
Aditya Verma <129338439+Aditya7verma@users.noreply.github.com>
2024-08-27 20:50:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ const port = process.env.PORT || 5000;
 
 const path = require("path");
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+
 app.listen(port, () => {
   console.log("Server is up at Port:- ", port);
 });
